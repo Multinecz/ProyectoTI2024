04a56fe779e6ee39ec564d757befbeb8e4bc5e90
Multinecz <nicolas.cepeda@alumnos.upla.cl>
2024-07-06 18:03:40 -0400

    Agrego Flask_app.py
    Está vinculará la aplicación de jupyter notebook

---
+++ flask_app.py
@@ -0,0 +1,45 @@
+import webbrowser
+import joblib
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+def detection(model, x):
+    x = str(x)
+    language = model.predict([x])[0]
+    if len(x) <= 20:
+        language = 'frase demasiado corta (<25 chars.)'
+    elif language == 'fr':
+        language = 'francés'
+    elif language == 'pt':
+        language = 'portugués'
+    elif language == 'it':
+        language = 'italiano'
+    elif language == 'en':
+        language = 'inglés'
+    elif language == 'es':
+        language = 'español'
+    return language
+
+
+@app.route('/', methods=['POST', 'GET'])
+def main_app():
+    try:
+        result = request.form
+        if request.method == 'POST':
+            dict_result = dict(result)
+            text = dict_result['text_to_language']
+            lang = detection(model, text)
+        else:
+            lang = None
+        return render_template('index.html', result={'language': lang.upper()})
+    except:
+        return render_template('index.html', result={'language': None})
+
+
+if __name__ == '__main__':
+    model = joblib.load('models/model.pkl')
+    url = "http://localhost:5000"
+    webbrowser.open_new(url)
+    app.run()
